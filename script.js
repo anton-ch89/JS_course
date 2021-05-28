7505a458d6aef3d6a72cba035f669cd2895df9df
Anton Chudinov <anton.ch89@gmail.com>
2021-05-28 12:13:03 +0500

    Домашка по уроку 2

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+let money = 2000;
+let income = 'фриланс';
+let addExpenses = 'Коммуналка, Мобильная связь, Интернет, Такси, Продукты';
+let deposit = true;
+let mission = 10000;
+let period = 10;
+let budgetDay = money/30;
 
-
-let money;
-let income;
-let addExpenses;
-let deposit;
-let middion;
-let period;
-
-alert('Hello world!');
-console.log('Hello World!');
-
-
+console.log('money: ', money);
+console.log('income: ', income);
+console.log('deposit: ', deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев.');
+console.log('Цель заработать ' + mission + ' долларов.');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('budgetDay: ', budgetDay);
 
